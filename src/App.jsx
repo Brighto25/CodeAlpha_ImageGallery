cc0c0e5a6a80c02ac297ebbd208f6035d0f33889
Brighto25 <Bright.Addaneyf3@gmail.com>
2026-06-08 11:59:46 +0000

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import {useState} from 'react'
+
+function App() {
+  const [images, setImages] = useState([]);
+  const [lightbox, setLightbox] = useState(null);
+
+  return <div></div>;
+}
+
+export default App;
